86dd2ae1a92b8ba747cb6edfbb828ab00105a5cc
Gashanas <gashanas@gmail.com>
2018-04-29 19:45:54 +0300

    test

--- app.js
+++ app.js
@@ -1,34 +1,35 @@
 const express = require('express');
 const app = express();
 
-const {Client} = require('pg');
+// const {Client} = require('pg');
+//
+// const client = new Client({
+//     connectionString: 'postgres://postgres:1234@localhost:5432/postgres',
+//     ssl: false,
+// });
+// var resp;
 
-const client = new Client({
-    connectionString: 'postgres://postgres:1234@localhost:5432/postgres',
-    ssl: false,
-});
-var resp;
-
-client.connect();
+// client.connect();
 //var db = pgp('postgres://postgres:1234@localhost:5432/postgres')
 
 //var pgp = require('pg-promise')(/*options*/)
 //var db = pgp('postgres://username:password@host:port/database')
 
 app.get('/', (req, res) => {
-    client.query('SELECT * FROM customers;', (err, res) => {
-        if (err) throw err;
-        resp = res.rows;
-        for (let row of res.rows) {
-            console.log(JSON.stringify(row));
-        }
-        client.end()
-    });
+    // client.query('SELECT * FROM customers;', (err, res) => {
+    //     if (err) throw err;
+    //     resp = res.rows;
+    //     for (let row of res.rows) {
+    //         console.log(JSON.stringify(row));
+    //     }
+    //     client.end()
+    // });
 
-    res.send(resp || 'sorry')
+    // res.send(resp || 'sorry')
+    res.send('sorry')
 });
 
-app.listen(process.env.PORT || 5000, () => console.log('Example app listening on port 3000!'))
+app.listen(process.env.PORT || 3000, () => console.log('Example app listening on port 3000!'));
 
 /*app.get('/', (req, res) => {
  db.one('SELECT $1 AS value', 123)
