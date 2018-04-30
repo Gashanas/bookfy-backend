a035a1ec4953e26bb7927e845550cf92994fd125
Gashanas <gashanas@gmail.com>
2018-05-01 01:28:15 +0300

    feat: created delete get methods

--- routes/index.js
+++ routes/index.js
@@ -7,97 +7,73 @@ const path = require('path');
 const connectionString = process.env.DATABASE_URL || 'postgres://szxdlmfkqhdcfv:e597d29f6b9a05f20933ef93adfaaa3aea0b0f2972bd74f3687d5484d6ea2f8f@ec2-54-163-240-54.compute-1.amazonaws.com:5432/d1oc2i36h8pqu5?ssl=true';
 //    connectionString =                             "postgres://*USERNAME*:*PASSWORD*@*HOST*:*PORT*/*DATABASE*"
 
-/* GET home page. */
-router.post('/api/v1/books', (req, res, next) => {
+
+router.post('/api/v1/book', (req, res, next) => {
     const results = [];
     // Grab data from http request
-    console.log(req.body.test, 'DATA1');
-    console.log(req.body, 'DATA2');
-    console.log(req.params, 'DATA3');
     if (req.body.title) {
         const data = req.body;
-        // res.send('hi');
-        // Get a Postgres client from the connection pool
+        // Get a Postgres client from the connection
         pg.connect(connectionString, (err, client, done) => {
-            // console.log(req, 'data');
-            // Handle connection errors
             if (err) {
                 done();
-                console.log(err);
                 return res.status(500).json({success: false, message: err});
             }
-            // SQL Query > Insert Data
-            const value = 'test3';
             client.query('INSERT INTO books (title) values ($1)', [data.title]);
-            // SQL Query > Select Data
-            const query = client.query('SELECT * FROM books', (err, res) => {
-                if (err) throw err;
-                resp = res.rows;
-            });
-            // // Stream results back one row at a time
-            query.on('row', (row) => {
-                results.push(row);
-            });
-            // After all data is returned, close connection and return results
-            // query.on('end', () => {
-            // res.send({ title: 'Express' });
-            done();
-            return res.json(results);
-            // });
+            getAllBooksAndReturn(client, res);
         });
     } else {
-        return res.status(400).json({success: false, data: "Please provide book information"})
+        return res.status(400).json({success: false, message: "Please provide book information"})
     }
 });
 
-router.get('/api/v1/todos', (req, res, next) => {
+router.get('/api/v1/books', (req, res, next) => {
     const results = [];
-    // Get a Postgres client from the connection pool
     pg.connect(connectionString, (err, client, done) => {
-        // Handle connection errors
         if (err) {
             done();
-            console.log(err);
-            return res.status(500).json({success: false, data: err});
+            return res.status(500).json({success: false, message: err});
         }
-        // SQL Query > Select Data
-        const query = client.query('SELECT * FROM items ORDER BY id ASC;');
-        // Stream results back one row at a time
-        query.on('row', (row) => {
-            results.push(row);
-        });
-        // After all data is returned, close connection and return results
-        query.on('end', () => {
-            done();
-            return res.json(results);
-        });
+        getAllBooksAndReturn(client, res);
     });
 });
 
-// router.get('/api/v1/todos', (req, res, next) => {
-//     const results = [];
-//     // Get a Postgres client from the connection pool
-//     pg.connect(connectionString, (err, client, done) => {
-//         // Handle connection errors
-//         if(err) {
-//             done();
-//             console.log(err);
-//             return res.status(500).json({success: false, data: err});
-//         }
-//         // SQL Query > Select Data
-//         const query = client.query('SELECT * FROM items ORDER BY id ASC;');
-//         // Stream results back one row at a time
-//         query.on('row', (row) => {
-//             results.push(row);
-//         });
-//         // After all data is returned, close connection and return results
-//         query.on('end', () => {
-//             done();
-//             return res.json(results);
-//         });
-//     });
-// });
-//
+router.delete('/api/v1/book', (req, res, next) => {
+    const results = [];
+    if(!req.body.id){
+        return res.status(400).json({success: false, message: "Please provide book id"});
+    } else if (!isNumeric(parseInt(req.body.id))){
+        return res.status(400).json({success: false, message: "Invalid id, it should be a number"});
+    } else {
+        const id = req.body.id;
+        pg.connect(connectionString, (err, client, done) => {
+            if (err) {
+                done();
+                return res.status(500).json({success: false, message: err});
+            }
+            client.query('DELETE FROM books WHERE id=($1)', [id]);
+            getAllBooksAndReturn(client, res);
+        });
+    }
+});
+
+function getAllBooksAndReturn(client, res) {
+    const results = [];
+    client.query('SELECT * FROM books', (err, res) => {
+        if (err) throw err;
+        for (let row of res.rows) {
+            results.push(row);
+        }
+    })
+        .then(() => {
+            return res.json(results);
+        })
+}
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Express'});
 });
